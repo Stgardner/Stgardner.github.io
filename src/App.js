83ae84d9e55257739dfa53b58db7bac99df49556
Stuart Gardner <stuarttgardner@gmail.com>
2021-08-19 15:27:15 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Testimonials from "./Components/Testimonials";
+import Portfolio from "./Components/Portfolio";
+
+import "./App.css";
+
+const App = () => {
+  const [resumeData, setResumeData] = useState({});
+
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Portfolio data={resumeData.portfolio} />
+      <Testimonials data={resumeData.testimonials} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
+  );
+};
+
+export default App;

d3523967df34c5e8e5c3561f18f019f046561276
Stuart Gardner <stuarttgardner@gmail.com>
2021-09-14 15:28:53 -0300

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,10 +5,6 @@ const About = ({ data }) => {
     var name = data.name;
     var profilepic = "images/" + data.image;
     var bio = data.bio;
-    var street = data.address.street;
-    var city = data.address.city;
-    var state = data.address.state;
-    var zip = data.address.zip;
     var phone = data.phone;
     var email = data.email;
     var resumeDownload = data.resumedownload;
@@ -35,7 +31,7 @@ const About = ({ data }) => {
                 <span>{name}</span>
                 <br />
                 <span>
-                  {street}
+                 
                   <br />
                   
                 </span>
